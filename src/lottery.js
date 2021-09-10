949eeb8efdc020c9b71b7a1b47866667fdb5c5fc
femi omoshona <femostic4jesus@gmail.com>
2021-09-10 01:52:17 -0700

    initial commit

---
+++ src/lottery.js
@@ -0,0 +1,10 @@
+import web3 from './web3';
+
+
+const address = '0x5a01c07e7d4b91A22d145A390cdEb222e499Fd9d';
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+
+export default new web3.eth.Contract(abi, address);
+ 
